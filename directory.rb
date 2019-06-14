4419c6de31fb879450f35546929f2939a0761eba
EC2 Default User <ec2-user@ip-172-31-42-207.us-east-2.compute.internal>
2019-06-14 15:24:59 +0000

    Added solution to exercise 2

--- directory.rb
+++ directory.rb
@@ -1,36 +1,55 @@
-# Let's put all students into an array
 def input_students
     puts "Please enter the names of the students"
     puts "To finish, just hit return twice"
-    # create an empty array
     students = []
-    # get the first name
     name = gets.chomp
-    # while the name is not empty, repeat this code
     while !name.empty? do
-        # add the student hash to the array
         students << {name: name, cohort: :november} 
         puts "Now we have #{students.count} students"
-        # get another name from the user
         name = gets.chomp
     end
-    # return the array of students
     students
 end
+
 def print_header
     puts "The students of Villians Academy"
     puts "-------------"
 end
-def print(students)
-    students.each do |student|
-        puts "#{student[:name]} (#{student[:cohort]} cohort)"
+
+def print_specific_letter
+    loop do
+        puts "Would you like only print students beginning with a specific letter? y/n"
+        ui = gets.chomp
+        if ui.downcase == "n"
+            return 0
+        elsif ui.downcase == "y"
+            puts "Please input the letter"
+            ui_letter = gets.chomp
+            return ui_letter
+        else
+            puts "Input not recognized"
+        end
     end
 end
+
+def print(students)
+    if @temp == 0
+        students.each_with_index do |student, index|
+            puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+        end
+    else
+        students.each_with_index do |student, index|
+            puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].chr == @temp 
+        end
+    end
+end
+
 def print_footer(students)
     puts "Overall, we have #{students.count} great students"
 end
-#nothing happens until we call the methods
+
 students = input_students
+@temp = print_specific_letter
 print_header
 print(students)
 print_footer(students)
