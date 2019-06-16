8c38b2df829ce7eca118510aae20cd83fb658096
EC2 Default User <ec2-user@ip-172-31-42-207.us-east-2.compute.internal>
2019-06-16 16:42:41 +0000

    Aligned the output list

--- directory.rb
+++ directory.rb
@@ -1,54 +1,66 @@
 def input_students
-    puts "Please enter the names of the students."
+    puts "Please enter the name of the first student."
     puts "To finish, just hit return twice."
     students = []
+    tracker = 0
     name = gets.chomp
     while !name.empty? do
         students << {name: name, cohort: :november} 
         puts "Now we have #{students.count} students!"
+        puts "Please enter any hobbies #{students[tracker][:name]} has."
+        hobby = gets.chomp
+        students[tracker][:hobbies] = hobby
+        puts "Please enter #{students[tracker][:name]}'s country of birth."
+        country = gets.chomp
+        students[tracker][:birth_country] = country
+        puts "Please enter #{students[tracker][:name]}'s height."
+        height = gets.chomp
+        students[tracker][:height] = height
+        tracker += 1
+        puts "Please enter the name of the next student, or press enter to finish."
         name = gets.chomp
     end
     students
 end
 
 def print_header
-    puts "The students of Villians Academy"
-    puts "--------------------------------"
+    puts "The students of Villians Academy".center(50)
+    puts "------------------".center(50)
+end
+
+def print(students)
+    students.each_with_index do |student, index|
+        puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(50)
+    end
 end
 
 =begin
 def print(students)
-    students.each_with_index do |student, index|
-        puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    tracker = 0
+    while tracker < students.length
+        puts "#{tracker + 1}. #{students[tracker][:name]} (#{students[tracker][:cohort]} cohort)".center(50)
+        tracker += 1
     end
 end
 =end
-
-def print(students)
-    tracker = 0
-    while tracker < students.length
-    puts "#{tracker + 1}. #{students[tracker][:name]} (#{students[tracker][:cohort]} cohort)"
-    tracker += 1
-    end
-end
 
 def print_specific_letter(students)
     puts "Please enter the letter."
     psl_ui = gets.chomp
     print_header
     students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].chr == psl_ui
+        puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(50) if student[:name].chr == psl_ui
     end
 end
 
 def print_shortmode(students)
     students.each_with_index do |student, index|
-    puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)" if student[:name].length < 12
+        puts "#{index + 1}. #{student[:name]} (#{student[:cohort]} cohort)".center(50) if student[:name].length < 12
     end
 end
 
 def print_footer(students)
-    puts "Overall, we have #{students.count} great students"
+    puts "Overall, we have #{students.count} great students".center(50)
 end
 
 def prog_runner
